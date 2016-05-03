58497b257377be5380cc387298fc0bd91502d324
dungvatoi12 <dungvatoi12@gmail.com>
2016-05-03 12:29:38 +0700

    update 2

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ var port = Number(process.env.PORT || 3000);
 var uristring =
   process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
-  'mongodb://localhost:27017/';
+  'mongodb://localhost/';
 
 mongoose.connect (uristring, function (err) {
- 	if(err) throw err;
+ 	//if(err) throw err;
  	console.log('Coneccted!');
  	routes(app);
  	app.listen(port,function (req, res) {
